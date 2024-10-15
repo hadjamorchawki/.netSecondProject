61fffe58e74953edc7b8c8c542686c8dca6ae62b
hadjamorchawki <hadjamorchaouki@gmail.com>
2024-10-15 22:02:21 +0100

    init

---
+++ secondproject/Controllers/ProductController.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using secondproject.Models;
+using secondproject.Models.Repositories;
+using secondproject.ViewModels;
+using System.IO;
+using System;
+
+namespace secondproject.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IRepository<Product> _productRepository;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public ProductController(IRepository<Product> productRepository, IWebHostEnvironment hostingEnvironment)
+        {
+            _productRepository = productRepository;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public ActionResult Index()
+        {
+            var products = _productRepository.GetAll();
+            return View(products);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var product = _productRepository.Get(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = null;
+
+                if (model.ImagePath != null)
+                {
+                    string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "images");
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ImagePath.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        model.ImagePath.CopyTo(fileStream);
+                    }
+                }
+
+                Product newProduct = new Product
+                {
+                    Désignation = model.Désignation,
+                    Prix = model.Prix,
+                    Quantite = model.Quantite,
+                    Image = uniqueFileName
+                };
+
+                _productRepository.Add(newProduct);
+                return RedirectToAction(nameof(Details), new { id = newProduct.Id });
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Product product = _productRepository.Get(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            EditViewModel productEditViewModel = new EditViewModel
+            {
+                Id = product.Id,
+                Désignation = product.Désignation,
+                Prix = product.Prix,
+                Quantite = product.Quantite,
+                ExistingImagePath = product.Image
+            };
+
+            return View(productEditViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(EditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Product product = _productRepository.Get(model.Id);
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
+                product.Désignation = model.Désignation;
+                product.Prix = model.Prix;
+                product.Quantite = model.Quantite;
+
+                if (model.ImagePath != null)
+                {
+                    if (model.ExistingImagePath != null)
+                    {
+                        string filePath = Path.Combine(_hostingEnvironment.WebRootPath, "images", model.ExistingImagePath);
+                        System.IO.File.Delete(filePath);
+                    }
+
+                    product.Image = ProcessUploadedFile(model);
+                }
+
+                _productRepository.Update(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        [NonAction]
+        private string ProcessUploadedFile(EditViewModel model)
+        {
+            string uniqueFileName = null;
+            if (model.ImagePath != null)
+            {
+                string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ImagePath.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.ImagePath.CopyTo(fileStream);
+                }
+            }
+            return uniqueFileName;
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var product = _productRepository.Get(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            var product = _productRepository.Delete(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ secondproject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace secondproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
